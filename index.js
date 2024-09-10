fe1eec0731bb78746c65fdbd6267766420d8e767
Matheus <matheustoscano2012@gmail.com>
2024-09-10 14:49:19 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// ola, mundo!
-const mensagem = "olá, mundo!"
-console.log(mensagem);
+// arrays, objetos
+let meta = {
+    value: 'ler um livro todo mês',
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.value = "não é mais ler um livro"
+meta.log(meta.value)
+
+// function // arrow function
+// const criarMeta = () => {}
+
+// function criarMeta () {}
