77bbbf78cef468a2c50564c6b7584e4eb2b258d7
Matheus <matheustoscano2012@gmail.com>
2024-09-10 14:51:12 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // arrays, objetos
 let meta = {
     value: 'ler um livro todo mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
+    checked: true,
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
 
-// function // arrow function
-// const criarMeta = () => {}
-
-// function criarMeta () {}
+console.log(meta.value)
